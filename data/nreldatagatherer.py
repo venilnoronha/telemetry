97159f6da8c65e1e565b6cf0ba7e41b47b301749
Paul An <hakuliu@gmail.com>
2016-01-08 14:11:14 -0800

    changed some of the specifics of the parameters to give us los angeles info, as well as hourly data.

--- data/nreldatagatherer.py
+++ data/nreldatagatherer.py
@@ -4,7 +4,12 @@ import httplib
 Gotta make a HTTP request to the nrel database to get some data on the projected solar output of our car.
 '''
 def testconnection():
-    requeststring = "/api/pvwatts/v5.json?api_key=DEMO_KEY&lat=40&lon=-105&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10"
+    apibegin = '/api/pvwatts/v5.json?'
+    apikey = 'api_key=vSnjhMa0wAcb8PoIEQVqmRHd0fZJsek6QBcgw01r'#this is a key that uniquely identifies us. It doesn't really mean anything other than we need to have one.
+    locationparams = '&lat=34.02&lon=-118.28'
+    timeframeparam = '&timeframe=hourly'#if you don't include this, then it'll automatically give you monthly without the hours.
+    remainingparams = '&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10'#stuff i dont' really care about for now so i just went with the default from example
+    requeststring = apibegin+apikey+locationparams+timeframeparam+remainingparams
     c = httplib.HTTPSConnection("developer.nrel.gov")
     c.request("GET", requeststring)
     response = c.getresponse()
