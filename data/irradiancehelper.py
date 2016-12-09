e167938dd0150af400ae55076bccc84a467d0693
Paul <hakuliu@gmail.com>
2016-12-09 13:25:36 -0800

    playing around with irradiance calculator....
    i suck at physics T_T

--- data/irradiancehelper.py
+++ data/irradiancehelper.py
@@ -1,5 +1,9 @@
 __author__ = 'paul'
 import httplib
+import math
+import numpy
+from PIL import Image
+
 '''
 Gotta make a HTTP request to the nrel database to get some data on the projected solar output of our car.
 '''
@@ -17,5 +21,44 @@ def testconnection():
     data = response.read()
     print data
 
+def getirradiance(longitude, latitude):
+    lonr = degreetorad(longitude)
+    latr = degreetorad(latitude)
+    #constants
+    S0 = 1367
+    obliquity = 23.4398
+    eccen = 0.016704
+    perihelion = 282.895
+    declination = degreetorad(23)
+    h0 = math.pi
+
+    R0RE = 1 + math.e * math.cos(lonr + perihelion)
+
+    Q = (S0 / math.pi) * R0RE * (h0 * math.sin(latr) * math.sin(declination) + math.cos(latr) * math.cos(declination) * math.sin(h0))
+
+
+    costheta = math.cos(lonr) * math.sin(latr)
+    return Q
+
+def testirradiance():
+    data = numpy.ndarray((180,360), int)
+    max = 0
+    for x in range(0, 360, 1):
+        for y in range(-90, 90, 1):
+            val = getirradiance(x, y)
+            if(val > max):
+                max = val
+            data[y+90,x] = int(255*val / 5022.70)#y for row....x for col...
+    im = Image.fromarray(data)
+    im.show();
+    print(max)
+
+
+
+
+def degreetorad(d):
+    return d * math.pi / 180
+
+
 if __name__ == '__main__':
-    testconnection()
+    testirradiance()
