e4e06a2057ecdff2727588dce5080864f1c815b5
Paul An <hakuliu@gmail.com>
2017-01-09 17:02:58 -0800

    Made a quick-and-dirty implementation of zoom. perhaps jen can improve on this later.

--- sim/ui/simresultscreen.py
+++ sim/ui/simresultscreen.py
@@ -8,15 +8,18 @@ import os
 from kivy.uix.slider import Slider
 
 class SimResultScreen(BoxLayout):
+
     def __init__(self, parentapp, simobj):
+        MINZOOM = 1;
+        MAXZOOM = 10;
         super (SimResultScreen, self).__init__(orientation='vertical')
         self.parentapp = parentapp
         self.simobj = simobj
-        graphmodule = self.getGraphModule(simobj)
-        self.zoomslider = Slider(orientation='horizontal', min=-100, max=100, value=0, size_hint=(1, .1))
+        self.graphmodule = self.getGraphModule(simobj)
+        self.zoomslider = Slider(orientation='horizontal', min=MINZOOM, max=MAXZOOM, value=1, size_hint=(1, .1))
         self.zoomslider.bind(value=self.onZoomSliderChange)
 
-        self.add_widget(graphmodule)
+        self.add_widget(self.graphmodule)
         self.add_widget(self.zoomslider)
 
         bottombar = BoxLayout(orientation='horizontal',size_hint=(1.0,0.1))
@@ -52,7 +55,7 @@ class SimResultScreen(BoxLayout):
         files.datadump.dumpdata(os.path.join(targetDirectory, 'testdump.csv'), data)
 
     def onZoomSliderChange(self, instance, value):
-        print('my value changed to %s' % value)
+        self.graphmodule.setZoom(value)
         return;
 
 
@@ -61,11 +64,11 @@ class SimUnitPlot(Graph):
     numPlots=0
     isPlotted=False
     def __init__(self, title, solarres):
-        bounds=solarres.getMinMax()
+        self.bounds=solarres.getMinMax()
         Graph.__init__(self,
         x_ticks_major=60*60*60*24, y_ticks_major=500000,
         y_grid_label=True, x_grid_label=True, padding=5,
-        x_grid=True, y_grid=True, xmin=bounds[0], xmax=bounds[1], ymin=bounds[2], ymax=bounds[3],
+        x_grid=True, y_grid=True, xmin=self.bounds[0], xmax=self.bounds[1], ymin=self.bounds[2], ymax=self.bounds[3],
                        width=2000, height=500)
         self.unittype='test'
         tempplotdata = solarres.hist
@@ -74,4 +77,22 @@ class SimUnitPlot(Graph):
         plot.points = tempplotdata
         self.add_plot(plot)
 
+        return
+
+
+    def getDataMinX(self):
+        return self.bounds[0]
+
+    def getDataMaxX(self):
+        return self.bounds[1]
+
+    def getDataMinY(self):
+        return self.bounds[2]
+
+    def getDataMaxY(self):
+        return self.bounds[3]
+
+    def setZoom(self, scalef):
+        #just a quick and dirty for now...well need to implement actual xmin/xmax windows and zooms onto middle?
+        self.xmax = self.getDataMaxX() / float(scalef)
         return
